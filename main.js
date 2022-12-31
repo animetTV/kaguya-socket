03d98458cc3e697dd11cc55caf2dc71522a85a99
Asad <asadbekasadbek@gmail.com>
2022-12-30 22:23:45 -0500

    dsa

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:3000",
-      "https://v3.animet.tv",
+      "https://v3.animet.tv/",
       "https://animet.tv",
     ],
   },
